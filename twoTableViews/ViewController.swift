7f79a8ffec90e057c5ec1de008ca5a61f262ee01
apcs2 <apcs2@p-162mba-02.d214.org>
2017-08-28 09:31:24 -0500

    no message

--- twoTableViews/ViewController.swift
+++ twoTableViews/ViewController.swift
@@ -8,18 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var selectedTableView: UITableView!
+    @IBOutlet weak var regularTableView: UITableView!
+var pizzaToppings = ["Pepperoni","Cheese","Sausage","Pineapple","Broccoli"]
+    var selectedToppings = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    
+      }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+       if tableView.isEqual(selectedTableView)
+       {
+        return selectedToppings.count
+        
+        }
+    if tableView.isEqual(regularTableView)
+    {
+        return pizzaToppings.count
+    
+        }
+    else
+    {
+        return 100000000000000
+        }
     }
+    
+    
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+     {
+        
+        if tableView.isEqual(selectedTableView)
+        {
+            let cell = selectedTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+          
+            cell.textLabel?.text = selectedToppings[indexPath.row]
+            
+            return cell
+            }
+
+        
+       if tableView.isEqual(regularTableView)
+       {
+        let Cell = regularTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        Cell.textLabel?.text = pizzaToppings[indexPath.row]
+        
+        return Cell
+        }
+        else
+        {
+    let cell = regularTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+          cell.textLabel?.text = "nope"
+            return cell
     }
+        }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+      if tableView.isEqual(regularTableView)
+      {
+        selectedToppings.append(pizzaToppings[indexPath.row])
+        selectedTableView.reloadData()
+        pizzaToppings.remove(at: indexPath.row)
+        regularTableView.reloadData()
+        }
+        if tableView.isEqual(selectedTableView)
+        {
+              pizzaToppings.append(selectedToppings[indexPath.row])
+            selectedToppings.remove(at: indexPath.row)
+            regularTableView.reloadData()
+            selectedTableView.reloadData()
 
+        }
+
+    }
 }
 
